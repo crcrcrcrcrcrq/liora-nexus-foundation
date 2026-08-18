c7b9fe36e9b276ca8bff7c37231ad9394e6c6e19
crcrcrcrcrcrq <cr7@cr7.se>
2026-08-18 21:34:44 +0200

    Update config.server.ts

--- src/lib/telegram/config.server.ts
+++ src/lib/telegram/config.server.ts
@@ -1,35 +1,18 @@
-export const botLanguage="pl";
-export const adminAllowlist=["1250521295"];
-export const statsAllowlist=["1250521295"];
+import { env } from "cloudflare:workers";
+
+export const botLanguage = "pl";
+export const adminAllowlist = ["1250521295"];
+export const statsAllowlist = ["1250521295"];
 
 export function botToken(_bot?: any): string {
-  try {
-    // Cloudflare Workers
-    // @ts-ignore
-    const { env } = require("cloudflare:workers");
-    return env.TELEGRAM_BOT_TOKEN || env.BOT_TOKEN || env.TELEGRAM_ADMIN_BOT_TOKEN || "";
-  } catch {}
-  // @ts-ignore
-  if (typeof process !== "undefined" && process.env) {
-    // @ts-ignore
-    return process.env.TELEGRAM_BOT_TOKEN || process.env.BOT_TOKEN || "";
-  }
-  return "";
+  const e = env as any;
+  return e.TELEGRAM_BOT_TOKEN || e.BOT_TOKEN || e.TELEGRAM_ADMIN_BOT_TOKEN || "";
 }
 
 export function botWebhookSecret(_bot?: any): string {
-  try {
-    // @ts-ignore
-    const { env } = require("cloudflare:workers");
-    return env.TELEGRAM_BOT_WEBHOOK_SECRET || env.BOT_WEBHOOK_SECRET || env.TELEGRAM_BOT_WEBHC || "secret";
-  } catch {}
-  // @ts-ignore
-  if (typeof process !== "undefined" && process.env) {
-    // @ts-ignore
-    return process.env.TELEGRAM_BOT_WEBHOOK_SECRET || process.env.BOT_WEBHOOK_SECRET || "secret";
-  }
-  return "secret";
+  const e = env as any;
+  return e.TELEGRAM_BOT_WEBHOOK_SECRET || e.BOT_WEBHOOK_SECRET || e.TELEGRAM_BOT_WEBHC || e.TELEGRAM_BOT_WEBHOOK || "secret";
 }
 
-export const telegramConfig={botToken,adminAllowlist};
+export const telegramConfig = { botToken, adminAllowlist };
 export default telegramConfig;
